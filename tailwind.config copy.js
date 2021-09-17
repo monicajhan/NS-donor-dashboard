51cb655d80f350d9ed0ad180ac85ceee4b57704b
Monica <monica.jhan@gmail.com>
2021-09-17 10:23:08 -0400

    first commit

---
+++ tailwind.config copy.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+  container: {
+    center: true,
+    padding: '2rem'
+  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
